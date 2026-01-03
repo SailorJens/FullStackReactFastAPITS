6f56ddf59b89d502df1532311109b2c20ea3c9db
Jens Uwe Segler <jusegler@gmail.com>
2026-01-03 18:50:04 +0000

    working app

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # FullStackReactFastAPITS
+
+
+In backend, start virtual environment with 
+source .venv/bin/activate
+then start server with
+uvicorn app.main:app --reload --port 7777
+
+In frontend, start server with 
+npm run dev
--- backend/app/main.py
+++ backend/app/main.py
@@ -10,3 +10,13 @@ def health():
 @app.get("/hello")
 def hello():
     return {"message": "Hello from FastAPI"}
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
